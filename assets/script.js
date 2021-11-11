1626ac71f57d5dca0d3abe2c8250ad45ee64b011
Alissa Nordmoe <alissa.nordmoe@gmail.com>
2021-11-10 19:50:14 -0500

    async name in greeting

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
 function myDisplayer(some) {
-    document.getElementById("greeting").innerHTML = some;
-  }
-  
-  let name = document.getElementById("inputId").value; ;
+  document.getElementById("greeting").innerHTML = some;
+}
 
-  function onSubmit() {
-    document.querySelector('h1').innerHTML = name;
+let name = document.getElementById("inputId").value;
+let nameDisplay = document.querySelector("#nameDisplay");
 
-  }
+let btn;
+btn.addEventListener((click = () => {}));
 
-  
+async function addName() {
+  setTimeout(() => {
+    let submit = document.getElementById("inputId").value;
+
+    nameDisplay.innerHTML = submit;
+    nameDisplay.classList.add("animate__animated", "animate__bounceInRight");
+  }, 1000);
+}
+
+function onSubmit() {
+  // let submit = document.getElementById("inputId").value;
+  document.querySelector("#greeting").innerHTML = "top of the mornin... ";
+  addName();
+}
